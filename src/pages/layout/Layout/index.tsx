778fbba2d5dff7bf9e5332d9c325fdfd779bfa0b
sh7t <karimgaldonsky@gmail.com>
2026-02-02 13:45:21 +0200

    upd: react project! layout added.

---
+++ src/pages/layout/Layout/index.tsx
@@ -0,0 +1,26 @@
+import "./style.css";
+import React from "react";
+import Header from "../../../components/Header";
+import Footer from "../../../components/Footer";
+
+type LayoutProps = {
+    children?: React.ReactNode;
+}
+
+const Layout = (props: LayoutProps) => {
+    return (
+        <div className="page">
+            <Header />
+
+            <main className="main">
+                <div className="container">
+                    { props.children }
+                </div>
+            </main>
+
+            <Footer />
+        </div>
+    )
+}
+
+export default Layout;
