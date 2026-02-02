539b4890291c190c231c5425aefc8b3d3df53d73
sh7t <karimgaldonsky@gmail.com>
2026-02-02 13:50:57 +0200

    upd: routes added to main.tsx. also backend imitated with localStorage.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
+import {createRoot} from "react-dom/client";
+import {BrowserRouter, Route, Routes} from "react-router";
+import HomePage from "./pages/HomePage"
+import BooksPage from "./pages/BooksPage";
+import BookPage from "./pages/BookPage";
+import Layout from "./pages/layout/Layout";
+import AboutPage from "./pages/AboutPage";
+// import authors from "./fixtures/authors.json"
+//import books from "./fixtures/books.json"
+// import categories from "./fixtures/categories.json"
+
+// localStorage.setItem("authors", JSON.stringify(authors));
+// localStorage.setItem("books", JSON.stringify(books));
+// localStorage.setItem("categories", JSON.stringify(categories));
+
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Layout><HomePage /></Layout>}/>
+            <Route path="/about" element={<Layout><AboutPage /></Layout>}/>
+            <Route path="/books" element={<Layout><BooksPage /></Layout>}/>
+            <Route path="/book" element={ <Layout><BookPage /></Layout>}/>
+        </Routes>
+    </BrowserRouter>
 );
+
+
