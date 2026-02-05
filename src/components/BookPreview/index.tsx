9d4c679f7d4070067cfe62c3d007d505529ccad7
sh7t <karimgaldonsky@gmail.com>
2026-02-05 13:43:36 +0200

    fix: clickable preview added.

--- src/components/BookPreview/index.tsx
+++ src/components/BookPreview/index.tsx
@@ -1,4 +1,5 @@
 import "./style.css"
+import {useNavigate} from "react-router";
 
 type BookPreviewProps = {
     title: string;
@@ -7,19 +8,22 @@ type BookPreviewProps = {
 }
 
 const BookPreview = (props: BookPreviewProps) => {
+    const navigate = useNavigate();
     return (
         <div className="book-preview">
-            <div className="cover">
-                <img src={props.coverUrl} alt="Cover"/>
-            </div>
-            <div className="title">
-                <span>{props.title}</span>
-            </div>
-            <div className="author">
-                <span>{props.authorName}</span>
-            </div>
-            <div className="add-to-cart">
-                <button>Add to cart</button>
+            <div className="clickable-preview" onClick={() => navigate("/")}>
+                <div className="cover">
+                    <img src={props.coverUrl} alt="Cover"/>
+                </div>
+                <div className="title">
+                    <span>{props.title}</span>
+                </div>
+                <div className="author">
+                    <span>{props.authorName}</span>
+                </div>
+                <div className="add-to-cart">
+                    <button>Add to cart</button>
+                </div>
             </div>
         </div>
 
