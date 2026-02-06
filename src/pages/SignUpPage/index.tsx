92559ca73c89952393e13a3cafdf1b3a02c4f85a
sh7t <karimgaldonsky@gmail.com>
2026-02-07 01:56:28 +0200

    upd: sign up page design added.

--- src/pages/SignUpPage/index.tsx
+++ src/pages/SignUpPage/index.tsx
@@ -1,11 +1,35 @@
 import "./style.css"
 import Layout from "../layout/Layout";
+import React from "react";
+import { FaUserPlus } from "react-icons/fa";
+
+
 
 const SignUpPage = () => {
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+
+    };
     return (
         <Layout>
             <main className="main">
-                <p>Sign Up Page!</p>
+                <div className="sign-up-card">
+                    <div className="user-plus">
+                        <FaUserPlus size={40} color="#FFFFFF"/>
+                    </div>
+                    <h2>Create an account</h2>
+                    <h3>Enter your information to get started</h3>
+                    <form onSubmit={handleSubmit} className="sign-up-form">
+                        <label htmlFor="full-name">Full name:</label>
+                        <input type="text" id="full-name" name="full-name" placeholder="John Doe" required />
+                        <label htmlFor="email">Email:</label>
+                        <input type="email" id="email" name="email" placeholder="johndoe42@example.com" required/>
+                        <label htmlFor="password">Password:</label>
+                        <input type="password" id="password" name="password" placeholder="Enter password" pattern="^(?=.*\d)(?=.*[!@#$%^&*(),.?&quot;:{}|<>]).{8,}$" required />
+                        <input type="submit" value="Create account"/>
+                    </form>
+                    <h5 className="existing-account">Already have an account? <a href="/signin">Sign in</a></h5>
+                </div>
             </main>
         </Layout>
     );
