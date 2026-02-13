189feb82f16b4f3b48d3fc256b540ac019cd0053
sh7t <karimgaldonsky@gmail.com>
2026-02-13 20:57:58 +0200

    fix: routing changed due to login.

--- src/main.tsx
+++ src/main.tsx
@@ -10,13 +10,13 @@ import SignInPage from "./pages/SignInPage";
 import {checkToken} from "./services/localStorageHelper.ts";
 import NotFoundPage from "./pages/NotFoundPage";
 import ScrollToTop from "./components/ScrollToTop";
-// import authors from "./fixtures/authors.json"
-// import books from "./fixtures/books.json"
-// import categories from "./fixtures/categories.json"
+import authors from "./fixtures/authors.json"
+import books from "./fixtures/books.json"
+import categories from "./fixtures/categories.json"
 
-// localStorage.setItem("authors", JSON.stringify(authors));
-// localStorage.setItem("books", JSON.stringify(books));
-// localStorage.setItem("categories", JSON.stringify(categories));
+localStorage.setItem("authors", JSON.stringify(authors));
+localStorage.setItem("books", JSON.stringify(books));
+localStorage.setItem("categories", JSON.stringify(categories));
 
 
 createRoot(document.getElementById("root")!).render(
@@ -24,9 +24,11 @@ createRoot(document.getElementById("root")!).render(
         <ScrollToTop />
         <Routes>
             <Route path="/" element={<HomePage/>}/>
-            <Route path="/sign_up" element={<SignUpPage/>}/>
-            <Route path="/sign_in" element={<SignInPage/>}/>
             <Route path="/about_us" element={<AboutPage/>}/>
+
+            <Route path="/sign_up" element={!checkToken() ? <SignUpPage/> : <Navigate to="/"/>}/>
+            <Route path="/sign_in" element={!checkToken() ? <SignInPage/> : <Navigate to="/"/> }/>
+
             <Route path="/books" element={checkToken() ? <BooksPage/> : <Navigate to="/sign_in"/>}/>
             <Route path="/book/:bookId" element={checkToken() ? <BookPage/> : <Navigate to="/sign_in" />}/>
             <Route path="*" element={<NotFoundPage />} />
