3ce2ea5927bfc85ed24533b272c0f6562cc962a9
sh7t <karimgaldonsky@gmail.com>
2026-03-03 01:44:33 +0200

    upd: api test page added

---
+++ src/pages/ApiTestPage/index.tsx
@@ -0,0 +1,40 @@
+import "./style.css";
+import Layout from "../layout/Layout";
+import {useAppDispatch, useAppSelector} from "../../hooks/redux.ts";
+import {useEffect} from "react";
+import {getAllProducts} from "../../store/slices/productsSlice.ts";
+import Loader from "../../components/Loader";
+import BookPreview from "../../components/BookPreview";
+
+const ApiTestPage = () => {
+    const dispatch = useAppDispatch();
+    const {products, isLoading} = useAppSelector(state => state.products);
+
+    useEffect(() => {
+        dispatch(getAllProducts());
+    }, [dispatch]);
+    // definitely bad but still - just example
+    // definitely bad but still - just example
+    // definitely bad but still - just example
+    return (
+        <Layout>
+            <main className="main">
+                {isLoading && <Loader/>}
+                <div className="books-preview">
+                    {products.map(product => (
+                        <BookPreview
+                            key={product.id}
+                            id={product.id}
+                            title={product.title}
+                            variant={"buyable"}
+                            authorName={product.price.toString()}
+                            coverUrl={product.image}
+                        />
+                    ))}
+                </div>
+            </main>
+        </Layout>
+    );
+};
+
+export default ApiTestPage;
