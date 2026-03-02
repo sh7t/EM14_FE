53466d9f0efb8aa1672622c3fa3c7c9bf63ce3d9
sh7t <karimgaldonsky@gmail.com>
2026-03-03 01:34:28 +0200

    feat: products and axios integrated.

--- src/main.tsx
+++ src/main.tsx
@@ -1,57 +1,62 @@
 import "./index.css";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router";
+import {createRoot} from "react-dom/client";
+import {BrowserRouter, Navigate, Route, Routes} from "react-router";
 import HomePage from "./pages/HomePage";
 import BooksPage from "./pages/BooksPage";
 import BookPage from "./pages/BookPage";
 import AboutPage from "./pages/AboutPage";
 import SignUpPage from "./pages/SignUpPage";
 import SignInPage from "./pages/SignInPage";
-import { checkToken } from "./services/localStorageHelper.ts";
+import {checkToken} from "./services/localStorageHelper.ts";
 import NotFoundPage from "./pages/NotFoundPage";
 import ScrollToTop from "./components/ScrollToTop";
 import authors from "./fixtures/authors.json";
 import books from "./fixtures/books.json";
 import categories from "./fixtures/categories.json";
 import CartPage from "./pages/CartPage";
-import { Provider } from "react-redux";
-import { store } from "./store";
+import {Provider} from "react-redux";
+import {store} from "./store";
+import ApiTestPage from "./pages/ApiTestPage";
 
 localStorage.setItem("authors", JSON.stringify(authors));
 localStorage.setItem("books", JSON.stringify(books));
 localStorage.setItem("categories", JSON.stringify(categories));
 
 createRoot(document.getElementById("root")!).render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <ScrollToTop />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/about_us" element={<AboutPage />} />
+    <Provider store={store}>
+        <BrowserRouter>
+            <ScrollToTop/>
+            <Routes>
+                <Route path="/" element={<HomePage/>}/>
+                <Route path="/about_us" element={<AboutPage/>}/>
 
-        <Route
-          path="/sign_up"
-          element={!checkToken() ? <SignUpPage /> : <Navigate to="/" />}
-        />
-        <Route
-          path="/sign_in"
-          element={!checkToken() ? <SignInPage /> : <Navigate to="/" />}
-        />
+                <Route
+                    path="/sign_up"
+                    element={!checkToken() ? <SignUpPage/> : <Navigate to="/"/>}
+                />
+                <Route
+                    path="/sign_in"
+                    element={!checkToken() ? <SignInPage/> : <Navigate to="/"/>}
+                />
 
-        <Route
-          path="/cart"
-          element={checkToken() ? <CartPage /> : <Navigate to="/sign_in" />}
-        />
-        <Route
-          path="/books"
-          element={checkToken() ? <BooksPage /> : <Navigate to="/sign_in" />}
-        />
-        <Route
-          path="/book/:bookId"
-          element={checkToken() ? <BookPage /> : <Navigate to="/sign_in" />}
-        />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-    </BrowserRouter>
-  </Provider>,
+                <Route
+                    path="/cart"
+                    element={checkToken() ? <CartPage/> : <Navigate to="/sign_in"/>}
+                />
+                <Route
+                    path="/books"
+                    element={checkToken() ? <BooksPage/> : <Navigate to="/sign_in"/>}
+                />
+                <Route
+                    path="/book/:bookId"
+                    element={checkToken() ? <BookPage/> : <Navigate to="/sign_in"/>}
+                />
+                <Route
+                    path="/api-test"
+                    element={<ApiTestPage/>}
+                />
+                <Route path="*" element={<NotFoundPage/>}/>
+            </Routes>
+        </BrowserRouter>
+    </Provider>,
 );
